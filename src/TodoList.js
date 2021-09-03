cab4fd74a64622a90ab5ad1083b590bb2bd4bbb7
Sanket2604 <umaymailme2604@gmail.com>
2021-09-03 11:34:29 +0530

    v.1.1 Mob_view Btn fix

--- src/TodoList.js
+++ src/TodoList.js
@@ -4,7 +4,10 @@ import Todo from './Todo'
 export default function TodoList({todos, toggleTodo}) {
     if(todos.length ===0){
         return(
-            <div className="empty_todo">Your Todo List is Empty!!! Add Tasks To Todo List!!!</div>
+            <div className="empty_todo">
+                <p>Your Todo List is Empty!!!</p>
+                <p> Add Tasks To Todo List!!!</p>
+            </div>
         )
     }
     else{
